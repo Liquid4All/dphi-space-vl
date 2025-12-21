96f09498d91e6de4502be39e944cfe798930fd59
Liren Tu <tuliren@gmail.com>
2025-12-20 21:30:34 -0800

    Delete bash scripts

--- bin/build-orin-3b.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COMMIT_SHA=$(git rev-parse --short=10 HEAD)
-
-docker build -f Orin.Dockerfile -t liquidai/lfm2-vl-3b-gguf-q4:$COMMIT_SHA \
-  --build-arg HF_REPO=LiquidAI/LFM2-VL-3B-GGUF \
-  --build-arg TEXT_FILE=LFM2-VL-3B-Q4_0.gguf \
-  --build-arg MMPROJ_FILE=mmproj-LFM2-VL-3B-Q8_0.gguf \
-  .
