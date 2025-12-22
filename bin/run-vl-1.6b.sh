feb7a1db5f439385a85f376b035d5a93274f9f03
Liren Tu <1933157+tuliren@users.noreply.github.com>
2025-12-21 20:26:32 -0800

    Tag image with gh200 or orin (#5)
    * Tag image with gh200 or orin
    
    * Remove --push option
    
    * Update readme
    
    * Use python instead of python3

--- bin/run-vl-1.6b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker run --runtime nvidia --rm --network host liquidai/lfm2-vl-1p6b-gguf-q4:latest
--- bin/run-vl-3b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker run --runtime nvidia --rm --network host liquidai/lfm2-vl-3b-gguf-q4:latest
